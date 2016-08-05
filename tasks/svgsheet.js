73cef29f4c95cbd69579ae6f50a86d2ed598bfba
Philipp Antoni <pa@stripe.com>
2016-08-05 14:08:05 -0700

    Arcs

--- tasks/svgsheet.js
+++ tasks/svgsheet.js
@@ -15,71 +15,75 @@ module.exports = function(grunt) {
   var parse = require('svg-path-parser');
   var logSymbols = require('log-symbols');
   var ejs = require('ejs');
-  
-  
+
+
   function translateChildren(i, $el, filepath, tx, ty) {
     switch ($el.name) {
-      
+
       case 'g':
         $el.children.forEach(function($el, i){
           translateChildren(i, $el, filepath, tx, ty);
         });
         break;
-      
+
       case 'path':
         // Parse path and translate the individual commands,
         // then reassemble as a new path string
         var d = parse($el.attribs.d);
         var dnew = '';
         var args = ['x1','y1','x2','y2','x','y'];
-        
+
         d.forEach(function(cmd){
-          if (cmd.command.toLowerCase() === "a") {
-            grunt.log.writeln(logSymbols.error, filepath, ': Sorry, arcs are not implemented yet');
-          }
-        
           if (!cmd.relative) {
             args.forEach(function(arg){
               if (cmd[arg] != undefined) cmd[arg] += (arg[0] == 'x') ? tx : ty;
             });
           }
-          
+
+          function b(s) { return s ? 1 : 0 }
+
           dnew += cmd.code;
-          args.forEach(function(arg){
-            if (cmd[arg] != undefined) dnew += cmd[arg] + ' ';
-          });
+          if (cmd.code.toLowerCase() === "a") {
+            dnew += cmd.rx + ' ' + cmd.ry + ' '
+                  + cmd.xAxisRotation + ' ' + b(cmd.largeArc) + ' ' + b(cmd.sweep) + ' '
+                  + cmd.x + ' ' + cmd.y;
+          } else {
+            args.forEach(function(arg){
+              if (cmd[arg] != undefined) dnew += cmd[arg] + ' ';
+            });
+          }
         });
-          
+
         $el.attribs.d = dnew;
         break;
-        
+
       case 'circle':
         $el.attribs.cx = String( parseFloat($el.attribs.cx) + tx );
         $el.attribs.cy = String( parseFloat($el.attribs.cy) + ty );
         break;
-        
+
       case 'rect':
         $el.attribs.x = String( parseFloat($el.attribs.x) + tx );
         $el.attribs.y = String( parseFloat($el.attribs.y) + ty );
         break;
-        
+
       default:
         grunt.log.writeln(logSymbols.error, filepath, ': Sorry, ' + $el.name + ' is not implemented yet');
-        
+
     }
   }
-  
+
 
   grunt.registerMultiTask('svgsheet', 'Creates a sprite sheet out of SVGs.', function() {
-    
+
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       iconPadding: 10,
       iconDefaultWidth: 100,
       iconDefaultHeight: 100,
-      
+
       sheetMaxWidth: 500,
-      
+
       generateCss: true,
       generateHtml: true,
       cssTemplate: __dirname + '/../templates/css.ejs',
@@ -93,15 +97,15 @@ module.exports = function(grunt) {
       var fileWidth = 0,
           fileHeight = 0;
       var rowHeight = 0;
-    
+
       var destDir = path.dirname(f.dest),
           destName = path.basename(f.dest, '.svg');
-      
+
       var $resultDocument = cheerio.load('<svg xmlns="http://www.w3.org/2000/svg"></svg>', { xmlMode: true }),
           $resultSvg = $resultDocument('svg');
-          
+
       var icons = [];
-      
+
       // Process source files
       var src = f.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
@@ -112,32 +116,32 @@ module.exports = function(grunt) {
           return true;
         }
       }).map(function(filepath) {
-        
+
         var icon = {
           filename: path.basename(filepath, '.svg'),
         };
         icon.filenameSanitized = icon.filename.toLowerCase().replace(/\W+/g, "-");
 
         var contentStr = grunt.file.read(filepath);
-        var $ = cheerio.load(contentStr, { 
+        var $ = cheerio.load(contentStr, {
               normalizeWhitespace: true,
               xmlMode: true
             });
         var $svg = $('svg');
-        
+
         // Move all the elements in the SVG to their new position
         $svg.children().each(function(i, $el){
           translateChildren(i, $el, filepath, tx, ty);
         });
-            
+
         // Carry over attributes from the root SVG element
         var attrs = ['opacity','fill','fill-opacity','stroke','stroke-width','stroke-opacity'];
         var rootAttrs = [];
-        
+
         attrs.forEach(function(a){
           if ($svg.attr(a)) rootAttrs.push(a);
         })
-        
+
         if (rootAttrs.length) {
           var $g = $("<g></g>").html($svg.html());
           rootAttrs.forEach(function(a){
@@ -145,14 +149,14 @@ module.exports = function(grunt) {
           })
           $svg.html($g);
         }
-        
+
         // Add SVG to the sheet
         $resultSvg.append($svg.html());
-        
+
         icon.left = -tx;
         icon.top = -ty;
         icons.push(icon);
-        
+
         // Try to determine current SVG size
         if ($svg.attr('height')) {
           icon.height = parseInt( $svg.attr('height') );
@@ -160,17 +164,17 @@ module.exports = function(grunt) {
         }
         else if ($svg.attr('viewBox')) {
           var viewbox = $svg.attr('viewBox').split(' ');
-          
+
           icon.height = parseInt(viewbox[3]);
           icon.width = parseInt(viewbox[2]);
         }
         else {
           icon.height = options.iconDefaultHeight;
           icon.width = options.iconDefaultWidth;
-          
+
           grunt.log.writeln(logSymbols.warning, filepath, ': No width/height or viewBox found');
         }
-        
+
         // Move position along for the next file
         rowHeight = Math.max(rowHeight, icon.height);
 
@@ -181,7 +185,7 @@ module.exports = function(grunt) {
           tx = options.iconPadding;
           rowHeight = 0;
         }
-        
+
         fileWidth = Math.max(fileWidth, tx + icon.width + options.iconPadding);
         fileHeight = Math.max(fileHeight, ty + icon.height + options.iconPadding);
       });
@@ -189,31 +193,31 @@ module.exports = function(grunt) {
       // Write the SVG sheet
       $resultSvg.attr("width", fileWidth);
       $resultSvg.attr("height", fileHeight);
-      
+
       grunt.file.write(f.dest, $resultDocument.html());
       grunt.log.writeln(logSymbols.success, f.dest);
-      
+
       // Write the CSS
       var ejsData = {
         'icons': icons,
         'destName': destName
       }
-      
+
       if (options.generateCss) {
         var tpl = fs.readFileSync(options.cssTemplate, 'utf8')
         var out = ejs.render(tpl, ejsData);
         var outPath = path.join(destDir, destName + ".css");
-        
+
         grunt.file.write(outPath, out);
         grunt.log.writeln(logSymbols.success, outPath);
       }
-      
+
       // Write the HTML
       if (options.generateHtml) {
         var tpl = fs.readFileSync(options.htmlTemplate, 'utf8')
         var out = ejs.render(tpl, ejsData);
         var outPath = path.join(destDir, destName + ".html");
-        
+
         grunt.file.write(outPath, out);
         grunt.log.writeln(logSymbols.success, outPath);
       }
